44a9417f094ce24eb622ee55bfd6de4f706c1f07
ArtcalO97 <niyca14@gmail.com>
2020-09-29 18:08:47 +0200

    Adding Quote and QuotesArray

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
     <div class="container">
-    
+    	<app-quote-array :quotes="quotes"></app-quote-array>
     </div>
 </template>
 
 <script>
+	import QuotesArray from './components/QuotesArray.vue'
     export default {
-        
-    }
+
+        data (){
+        	return {
+        		quotes : [
+        			'WonderFul Quotes',
+        			'Holla Game over'
+        		],
+        		max : 10
+        	}
+        },
+
+        components : {
+        	appQuoteArray : QuotesArray
+        }
+    };
 </script>
 
 <style>
