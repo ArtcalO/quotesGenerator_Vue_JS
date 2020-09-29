4cb365db9d0573a44a9ec92c793a9110a4ad08d9
ArtcalO97 <niyca14@gmail.com>
2020-09-29 18:24:56 +0200

    raw files

--- src/components/QuotesArray.vue
+++ src/components/QuotesArray.vue
@@ -1,15 +1,18 @@
 <template>
 	<div class="row">
+		<app-add-quote></app-add-quote>
 		<app-quote v-for="quote in quotes">{{ quote }}</app-quote>
 	</div>
 </template>
 
 <script>
+	import AddQuote from './AddQuote.vue'
 	import Quote from './Quotes.vue'
 	export default {
 		props : ['quotes'],
 		components : {
-			appQuote : Quote
+			appQuote : Quote,
+			appAddQuote : AddQuote
 		}
 	};
 </script>
