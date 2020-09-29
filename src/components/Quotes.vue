0c8fcef5a66ca81af7507db474932824d8c6240e
ArtcalO97 <niyca14@gmail.com>
2020-09-29 18:09:05 +0200

    Quotes Components added

---
+++ src/components/Quotes.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="quote-card">
+		<div>
+			<div >
+				<slot></slot>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default{
+		props : ['quotes']
+	};
+</script>
+
+<style >
+	
+	.quote-card{
+		width: 30%;
+		height: 50px;
+		margin-right: 20px;
+		text-align: center;
+		padding-top: 8px;
+		margin-top: 10px;
+		background-color: gray;
+	}
+	.quote-card:hover{
+		cursor:pointer;
+		background-color: orange;
+	}
+
+</style>
