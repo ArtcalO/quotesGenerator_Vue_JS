c7bc1ab9d7498a65f25aa1bf238f9b4171816c68
ArtcalO97 <niyca14@gmail.com>
2020-09-29 18:25:21 +0200

    raw files added

---
+++ src/components/AddQuote.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="row">
+        <form>
+            <div class="col-sm-8 ">
+                <label class="form-control">Quote</label>
+                <textarea class="form-control" v-model="quote"></textarea>
+            </div>
+            <div class="col-sm-8 ">
+               <button class="btn btn-primary" @click.prevent="createQuote">Add Quote</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                quote: ''
+            };
+        },
+        methods: {
+            createQuote() {
+                this.$emit('quoteCreated', this.quote);
+                this.quote = '';
+            }
+        }
+    };
+</script>
+
+<style>
+
+</style>
