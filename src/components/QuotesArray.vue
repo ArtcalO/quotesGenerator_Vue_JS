3e38493b4aa3a72f41b8015ed39f7fc1d4c1dc1e
ArtcalO97 <niyca14@gmail.com>
2020-09-30 11:57:57 +0200

    Removing quote form in quootegrid

--- src/components/QuotesArray.vue
+++ src/components/QuotesArray.vue
@@ -1,18 +1,15 @@
 <template>
 	<div class="row">
-		<app-add-quote></app-add-quote>
 		<app-quote v-for="quote in quotes">{{ quote }}</app-quote>
 	</div>
 </template>
 
 <script>
-	import AddQuote from './AddQuote.vue'
 	import Quote from './Quotes.vue'
 	export default {
 		props : ['quotes'],
 		components : {
-			appQuote : Quote,
-			appAddQuote : AddQuote
+			appQuote : Quote
 		}
 	};
 </script>
