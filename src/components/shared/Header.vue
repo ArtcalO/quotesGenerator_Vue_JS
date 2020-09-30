66bdfe3f1dda52c4baa2061b7cc092cd538d9667
ArtcalO97 <niyca14@gmail.com>
2020-09-30 21:12:00 +0200

    Add Progress bar in header

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -4,10 +4,16 @@
 		  <h1>Quote Generator in Vue JS</h1>
 		  <p>Write a Quote and Post It</p>
 		</div>
+
+		<div class="progress"  style="height:40px">
+		  <div class="progress-bar progress-bar-striped" :style="{width: (quotes.length / max) * 100 + '%'}" style="height:40px">{{ quotes.length }} / {{max}}</div>
+		</div> 
 	</div>
 </template>
 <script >
-	
+	export default {
+		props : ['quotes','max']
+	};
 </script>
 
 <style>
