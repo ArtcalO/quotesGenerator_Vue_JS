c26bf98634eac1425a754a9ee42696613e8d6874
ArtcalO97 <niyca14@gmail.com>
2020-09-30 13:20:23 +0200

    Custom BS4 FOR QUOTE GRID

--- src/components/Quotes.vue
+++ src/components/Quotes.vue
@@ -1,11 +1,11 @@
 <template>
-	<div class="quote-card">
 		<div>
-			<div >
-				<slot></slot>
+			<div class="card">
+			  <div class="card-body">
+			  	<slot></slot>
+			  </div>
 			</div>
 		</div>
-	</div>
 </template>
 
 <script>
@@ -15,19 +15,5 @@
 </script>
 
 <style >
-	
-	.quote-card{
-		width: 30%;
-		height: 50px;
-		margin-right: 20px;
-		text-align: center;
-		padding-top: 8px;
-		margin-top: 10px;
-		background-color: gray;
-	}
-	.quote-card:hover{
-		cursor:pointer;
-		background-color: orange;
-	}
 
 </style>
