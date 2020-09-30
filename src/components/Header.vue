e0ecdd8b96e5fd444391ada03f6f0581ffd6e6d0
ArtcalO97 <niyca14@gmail.com>
2020-09-30 11:58:12 +0200

    Adding header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<div class="jumbotron text-center">
+		  <h1>Quote Generator in Vue JS</h1>
+		  <p>Write a Quote and Post It</p>
+		</div>
+	</div>
+</template>
+<script >
+	
+</script>
+
+<style>
+	
+</style>
