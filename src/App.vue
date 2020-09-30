f71df4a23c41c760b908079e3f0ec79d45cceaae
ArtcalO97 <niyca14@gmail.com>
2020-09-30 13:19:33 +0200

    Adding Append Method

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,50 @@
 <template>
-    <div class="container.fluid">
+    <div >
         <app-header></app-header>
         <hr>
-        <app-add-quote></app-add-quote>
+        <div >
+            <app-add-quote @quoteCreated="appendQuote"></app-add-quote>
+        </div>
+        
         <div>
-            <app-quote-array :quotes="quotes"></app-quote-array>
+            <app-quote-array :quotes="quotes" ></app-quote-array>
+        </div>
+
+        <div>
+            <app-footer></app-footer>
         </div>
     	
     </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
+    import Header from './components/shared/Header.vue'
+    import Footer from './components/shared/Footer.vue'
     import AddQuote from './components/AddQuote.vue'
 	import QuotesArray from './components/QuotesArray.vue'
     export default {
 
         data (){
         	return {
+                q:'',
         		quotes : [
-        			'WonderFul Quotes',
-        			'Holla Game over'
+        			'WonderFul Quotes'
         		],
         		max : 10
         	}
+        },
+        methods : {
+            appendQuote($emit){
+                this.quotes.push($emit);
+
+            }
         },
 
         components : {
         	appQuoteArray : QuotesArray,
             appAddQuote : AddQuote,
-            appHeader : Header
+            appHeader : Header,
+            appFooter : Footer
         }
     };
 </script>
