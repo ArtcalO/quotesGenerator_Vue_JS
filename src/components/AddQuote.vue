cd0c40774c7a6ce4cd9d761e07f67701f0f54c7f
ArtcalO97 <niyca14@gmail.com>
2020-09-30 13:19:55 +0200

    Custom BS4 in Add Quote . vue

--- src/components/AddQuote.vue
+++ src/components/AddQuote.vue
@@ -1,14 +1,26 @@
 <template>
-    <div class="row">
-        <form>
-            <div class="col-sm-8 ">
-                <label class="form-control">Quote</label>
-                <textarea class="form-control" v-model="quote"></textarea>
+    <div >
+        <section id="cover" class="min-vh-100">
+            <div id="cover-caption">
+                <div class="container">
+                    <div class="row text-white">
+                        <div class="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto text-center form">
+                            <h4 class="display-5 py-2 ">Write your quote here</h4>
+                            <div class="px-2">
+                                <form action="" class="justify-content-center">
+                                    <div class="form-group">
+                                        <label class="sr-only">Write your quotes</label>
+                                        <textarea class="form-control" v-model="quote" ></textarea>
+                                    </div>
+                                    
+                                    <button class="btn btn-success btn-lg mb-3" @click.prevent="addQuote" >Add Quote</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-sm-8 ">
-               <button class="btn btn-primary" @click.prevent="createQuote">Add Quote</button>
-            </div>
-        </form>
+        </section>
     </div>
 </template>
 
@@ -20,7 +32,7 @@
             };
         },
         methods: {
-            createQuote() {
+            addQuote() {
                 this.$emit('quoteCreated', this.quote);
                 this.quote = '';
             }
@@ -28,6 +40,33 @@
     };
 </script>
 
-<style>
+<style >
+    #cover {
+    background: #222 url('../img/cover.jpg') center center no-repeat;
+    background-size: cover;
+    height: 100%;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    position: relative;
+}
 
+#cover-caption {
+    width: 100%;
+    position: relative;
+    z-index: 1;
+}
+
+/* only used for background overlay not needed for centering */
+form:before {
+    content: '';
+    height: 100%;
+    left: 0;
+    position: absolute;
+    top: 0;
+    width: 100%;
+    background-color: rgba(0,0,0,0.3);
+    z-index: -1;
+    border-radius: 10px;
+}
 </style>
