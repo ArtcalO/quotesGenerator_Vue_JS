94a7f47dba3489105b6f9746a2d4640f40ff9298
ArtcalO97 <niyca14@gmail.com>
2020-09-30 13:20:53 +0200

    aDDING HEADER, FOOTER COM[PONENTS, AND SOME IMG STATIC FILE

---
+++ src/components/shared/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		  <button type="button" class="btn btn-primary btn-block">ArtcalO</button> 
+
+    </div>
+	</div>
+</template>
+<script >
+	
+</script>
+
+<style>
+	
+</style>
